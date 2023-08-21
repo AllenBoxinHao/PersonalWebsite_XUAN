30e432eb52bfab97c97ba138f264ddb36798cadf
AllenBoxinHao <allen7nella@gmail.com>
2023-08-21 17:37:09 +1000

    add in real contents & improve UI & smooth UI transition

--- js/main.js
+++ js/main.js
@@ -1,27 +1,56 @@
 function toggle() {
-  let collapsible = document.getElementById("collapsible");
-  collapsible.classList.toggle("collapsible--expanded");
+    let collapsible = document.getElementById("collapsible");
+    collapsible.classList.toggle("collapsible--expanded");
 }
 
 const menuItems = document.querySelectorAll(".menu__link");
 
 menuItems.forEach((menuItem) => {
-  menuItem.addEventListener("click", (e) => {
-    menuItems.forEach((item) => item.classList.remove("active"));
-    e.currentTarget.classList.add("active");
-  });
+    menuItem.addEventListener("click", (e) => {
+        menuItems.forEach((item) => item.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+    });
 });
 
-function filterProjects(p) {
-  const projects = document.querySelectorAll(".portfolio__items div");
-  let filter = p.target.dataset.filter;
-  if (filter === "*") {
-    projects.forEach((project) => project.classList.remove("hidden"));
-  } else {
-    projects.forEach((project) => {
-      project.classList.contains(filter)
-        ? project.classList.remove("hidden")
-        : project.classList.add("hidden");
+const navIinks = document.querySelectorAll(".nav__links");
+
+navIinks.forEach((navIink) => {
+    navIink.addEventListener("click", (e) => {
+        navIinks.forEach((item) => item.classList.remove("active"));
+        e.currentTarget.classList.add("active");
     });
-  }
-}
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const emailLink = document.getElementById("emailLink");
+
+    emailLink.addEventListener("click", function () {
+        const emailAddress = "xuanchang15@gmail.com";
+        const subject = "General Inquiry";
+        const body = "Hi, I came across your information on...";
+
+        const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(
+            subject
+        )}&body=${encodeURIComponent(body)}`;
+
+        const popupOptions = "width=600,height=500,resizable=yes,scrollbars=yes";
+        const popupWindow = window.open(mailtoLink, "EmailPopup", popupOptions);
+
+        if (!popupWindow) {
+            window.location.href = mailtoLink;
+        }
+    });
+});
